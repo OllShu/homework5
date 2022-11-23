f89c5f5f832d1d71e334f95ffe9598c2b7da7766
dns <shulgaol@mail.ru>
2022-11-23 11:19:52 +0200

    задание 1 второго урока про условные операторы

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int clientOS;
+        clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+    }
+}
