2b9f4c21cda6ea0e339876e416db4aa6ce9d1a20
dns <shulgaol@mail.ru>
2022-11-23 12:45:54 +0200

    задание 4 второго урока про условные операторы

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,9 @@ public class Main {
         if (clientOS == 0) {
             if (clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
             }
-                else {
-                    System.out.println("Установите версию приложения для iOS по ссылке");
-                }
         }
         if (clientOS == 1) {
             if (clientDeviceYear < 2015) {
@@ -26,12 +25,27 @@ public class Main {
         int year;
         year = 2022;
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            System.out.println(year+" год является високосным");
+            System.out.println(year + " год является високосным");
         } else {
-            System.out.println(year+" год не является високосным");
+            System.out.println(year + " год не является високосным");
         }
         System.out.println("_________________________________________");
 
+        // задание 4
+
+        int deliveryDistance;
+        int deliveryDay;
+        deliveryDistance = 100;
+
+        if (deliveryDistance < 20) {
+            deliveryDay = 1;
+        } else if ((deliveryDistance - 20) % 40 == 0) {
+            deliveryDay = 1 + (deliveryDistance - 20) / 40;
+        } else {
+            deliveryDay = 2 + (deliveryDistance - 20) / 40;
+        }
+
+        System.out.println("Доставка займет дней всего: " + deliveryDay);
 
     }
 }
