bfe88ef27da48e0475ed890d371e2313d65bdce4
dns <shulgaol@mail.ru>
2022-11-23 12:07:14 +0200

    задание 3 второго урока про условные операторы

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,19 @@ public class Main {
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
         }
+        System.out.println("_________________________________________");
+
+        // задание 3
+
+        int year;
+        year = 2022;
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year+" год является високосным");
+        } else {
+            System.out.println(year+" год не является високосным");
+        }
+        System.out.println("_________________________________________");
+
+
     }
 }
